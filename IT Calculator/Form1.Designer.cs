a61fb93e47d060d406b15482434746a6b667d9d8
William Bushie <willbushie@gmail.com>
2021-04-10 15:00:23 -0500

    Moved the updated Form1.Designer.cs to correct location.

--- IT Calculator/Form1.Designer.cs
+++ IT Calculator/Form1.Designer.cs
@@ -40,111 +40,241 @@ namespace IT_Calculator
             this.binaryArithm_AnswerTextBox = new System.Windows.Forms.TextBox();
             this.binaryArithm_OperationAnswerTextBoxLabel = new System.Windows.Forms.Label();
             this.binaryArithm_ProcessOperationButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // binaryArithmaticSectionLabel
             // 
             this.binaryArithmaticSectionLabel.AutoSize = true;
-            this.binaryArithmaticSectionLabel.Location = new System.Drawing.Point(14, 9);
+            this.binaryArithmaticSectionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.binaryArithmaticSectionLabel.Location = new System.Drawing.Point(476, 9);
+            this.binaryArithmaticSectionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.binaryArithmaticSectionLabel.Name = "binaryArithmaticSectionLabel";
-            this.binaryArithmaticSectionLabel.Size = new System.Drawing.Size(85, 13);
+            this.binaryArithmaticSectionLabel.Size = new System.Drawing.Size(178, 26);
             this.binaryArithmaticSectionLabel.TabIndex = 0;
             this.binaryArithmaticSectionLabel.Text = "Binary Arithmetic";
             // 
             // binaryArith_LeftTextBox
             // 
-            this.binaryArith_LeftTextBox.Location = new System.Drawing.Point(17, 63);
+            this.binaryArith_LeftTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.binaryArith_LeftTextBox.Location = new System.Drawing.Point(285, 97);
+            this.binaryArith_LeftTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.binaryArith_LeftTextBox.Name = "binaryArith_LeftTextBox";
-            this.binaryArith_LeftTextBox.Size = new System.Drawing.Size(100, 20);
+            this.binaryArith_LeftTextBox.Size = new System.Drawing.Size(148, 30);
             this.binaryArith_LeftTextBox.TabIndex = 1;
             // 
             // operationComboBox
             // 
+            this.operationComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.operationComboBox.FormattingEnabled = true;
             this.operationComboBox.Items.AddRange(new object[] {
             "Addition",
             "Subtraction",
             "Multiplication",
             "Division"});
-            this.operationComboBox.Location = new System.Drawing.Point(133, 63);
+            this.operationComboBox.Location = new System.Drawing.Point(490, 94);
+            this.operationComboBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.operationComboBox.Name = "operationComboBox";
-            this.operationComboBox.Size = new System.Drawing.Size(76, 21);
+            this.operationComboBox.Size = new System.Drawing.Size(152, 33);
             this.operationComboBox.TabIndex = 2;
             // 
             // binaryArithm_LeftTextBoxLabel
             // 
             this.binaryArithm_LeftTextBoxLabel.AutoSize = true;
-            this.binaryArithm_LeftTextBoxLabel.Location = new System.Drawing.Point(18, 47);
+            this.binaryArithm_LeftTextBoxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.binaryArithm_LeftTextBoxLabel.Location = new System.Drawing.Point(270, 57);
+            this.binaryArithm_LeftTextBoxLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.binaryArithm_LeftTextBoxLabel.Name = "binaryArithm_LeftTextBoxLabel";
-            this.binaryArithm_LeftTextBoxLabel.Size = new System.Drawing.Size(98, 13);
+            this.binaryArithm_LeftTextBoxLabel.Size = new System.Drawing.Size(183, 25);
             this.binaryArithm_LeftTextBoxLabel.TabIndex = 3;
             this.binaryArithm_LeftTextBoxLabel.Text = "First Binary Number";
             // 
             // operationComboBoxLabel
             // 
             this.operationComboBoxLabel.AutoSize = true;
-            this.operationComboBoxLabel.Location = new System.Drawing.Point(145, 47);
+            this.operationComboBoxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.operationComboBoxLabel.Location = new System.Drawing.Point(514, 57);
+            this.operationComboBoxLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.operationComboBoxLabel.Name = "operationComboBoxLabel";
-            this.operationComboBoxLabel.Size = new System.Drawing.Size(53, 13);
+            this.operationComboBoxLabel.Size = new System.Drawing.Size(98, 25);
             this.operationComboBoxLabel.TabIndex = 4;
             this.operationComboBoxLabel.Text = "Operation";
             // 
             // binaryArith_RightTextBox
             // 
-            this.binaryArith_RightTextBox.Location = new System.Drawing.Point(228, 63);
+            this.binaryArith_RightTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.binaryArith_RightTextBox.Location = new System.Drawing.Point(692, 97);
+            this.binaryArith_RightTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.binaryArith_RightTextBox.Name = "binaryArith_RightTextBox";
-            this.binaryArith_RightTextBox.Size = new System.Drawing.Size(100, 20);
+            this.binaryArith_RightTextBox.Size = new System.Drawing.Size(148, 30);
             this.binaryArith_RightTextBox.TabIndex = 5;
             // 
             // binaryArithm_RightTextBoxLabel
             // 
             this.binaryArithm_RightTextBoxLabel.AutoSize = true;
-            this.binaryArithm_RightTextBoxLabel.Location = new System.Drawing.Point(220, 47);
+            this.binaryArithm_RightTextBoxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.binaryArithm_RightTextBoxLabel.Location = new System.Drawing.Point(658, 57);
+            this.binaryArithm_RightTextBoxLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.binaryArithm_RightTextBoxLabel.Name = "binaryArithm_RightTextBoxLabel";
-            this.binaryArithm_RightTextBoxLabel.Size = new System.Drawing.Size(116, 13);
+            this.binaryArithm_RightTextBoxLabel.Size = new System.Drawing.Size(214, 25);
             this.binaryArithm_RightTextBoxLabel.TabIndex = 6;
             this.binaryArithm_RightTextBoxLabel.Text = "Second Binary Number";
             // 
             // conversionSectionLabel
             // 
             this.conversionSectionLabel.AutoSize = true;
-            this.conversionSectionLabel.Location = new System.Drawing.Point(17, 150);
+            this.conversionSectionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.conversionSectionLabel.Location = new System.Drawing.Point(507, 374);
+            this.conversionSectionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.conversionSectionLabel.Name = "conversionSectionLabel";
-            this.conversionSectionLabel.Size = new System.Drawing.Size(65, 13);
+            this.conversionSectionLabel.Size = new System.Drawing.Size(133, 26);
             this.conversionSectionLabel.TabIndex = 7;
             this.conversionSectionLabel.Text = "Conversions";
             // 
             // binaryArithm_AnswerTextBox
             // 
-            this.binaryArithm_AnswerTextBox.Location = new System.Drawing.Point(228, 109);
+            this.binaryArithm_AnswerTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.binaryArithm_AnswerTextBox.Location = new System.Drawing.Point(495, 246);
+            this.binaryArithm_AnswerTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.binaryArithm_AnswerTextBox.Name = "binaryArithm_AnswerTextBox";
-            this.binaryArithm_AnswerTextBox.Size = new System.Drawing.Size(100, 20);
+            this.binaryArithm_AnswerTextBox.Size = new System.Drawing.Size(148, 30);
             this.binaryArithm_AnswerTextBox.TabIndex = 8;
             // 
             // binaryArithm_OperationAnswerTextBoxLabel
             // 
             this.binaryArithm_OperationAnswerTextBoxLabel.AutoSize = true;
-            this.binaryArithm_OperationAnswerTextBoxLabel.Location = new System.Drawing.Point(256, 93);
+            this.binaryArithm_OperationAnswerTextBoxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.binaryArithm_OperationAnswerTextBoxLabel.Location = new System.Drawing.Point(530, 206);
+            this.binaryArithm_OperationAnswerTextBoxLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.binaryArithm_OperationAnswerTextBoxLabel.Name = "binaryArithm_OperationAnswerTextBoxLabel";
-            this.binaryArithm_OperationAnswerTextBoxLabel.Size = new System.Drawing.Size(45, 13);
+            this.binaryArithm_OperationAnswerTextBoxLabel.Size = new System.Drawing.Size(84, 25);
             this.binaryArithm_OperationAnswerTextBoxLabel.TabIndex = 9;
             this.binaryArithm_OperationAnswerTextBoxLabel.Text = "Answer:";
             // 
             // binaryArithm_ProcessOperationButton
             // 
-            this.binaryArithm_ProcessOperationButton.Location = new System.Drawing.Point(133, 107);
+            this.binaryArithm_ProcessOperationButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.binaryArithm_ProcessOperationButton.Location = new System.Drawing.Point(512, 149);
+            this.binaryArithm_ProcessOperationButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.binaryArithm_ProcessOperationButton.Name = "binaryArithm_ProcessOperationButton";
-            this.binaryArithm_ProcessOperationButton.Size = new System.Drawing.Size(75, 23);
+            this.binaryArithm_ProcessOperationButton.Size = new System.Drawing.Size(112, 46);
             this.binaryArithm_ProcessOperationButton.TabIndex = 10;
             this.binaryArithm_ProcessOperationButton.Text = "Equals";
             this.binaryArithm_ProcessOperationButton.UseVisualStyleBackColor = true;
             this.binaryArithm_ProcessOperationButton.Click += new System.EventHandler(this.binaryArithm_ProcessOperationButton_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(-6, 345);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1212, 5);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(264, 445);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(254, 30);
+            this.textBox1.TabIndex = 13;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Binary to Decimal",
+            "Binary to Hexadecimal",
+            "Decimal to Binary",
+            "Decimal to Hexadecimal",
+            "Hexadecimal to Binary",
+            "Hexadecimal to Decimal",
+            "Mac Address to Binary",
+            "Mac Address to Decimal",
+            "IP Address to Binary"});
+            this.comboBox1.Location = new System.Drawing.Point(633, 445);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(254, 33);
+            this.comboBox1.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(460, 622);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(228, 30);
+            this.textBox2.TabIndex = 15;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(512, 518);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 42);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Equals";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(342, 415);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 25);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Initial Base";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(669, 415);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 25);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Conversion Type";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(530, 591);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 25);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Answer:";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.binaryArithm_ProcessOperationButton);
             this.Controls.Add(this.binaryArithm_OperationAnswerTextBoxLabel);
             this.Controls.Add(this.binaryArithm_AnswerTextBox);
@@ -156,8 +286,10 @@ namespace IT_Calculator
             this.Controls.Add(this.operationComboBox);
             this.Controls.Add(this.binaryArith_LeftTextBox);
             this.Controls.Add(this.binaryArithmaticSectionLabel);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "IT Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -176,6 +308,14 @@ namespace IT_Calculator
         private System.Windows.Forms.TextBox binaryArithm_AnswerTextBox;
         private System.Windows.Forms.Label binaryArithm_OperationAnswerTextBoxLabel;
         private System.Windows.Forms.Button binaryArithm_ProcessOperationButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
