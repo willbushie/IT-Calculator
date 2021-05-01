08c3c3d67cb49a87c2b813f400174685352edb59
William Bushie <willbushie@gmail.com>
2021-04-30 23:31:27 -0500

    file storing and writing work | #3

--- IT Calculator/file.cs
+++ IT Calculator/file.cs
@@ -37,8 +37,9 @@ namespace IT_Calculator
     internal class file
     {
         /*
-         * This method will create the file structure necessary if it does not already exist in the
-         * Documents folder. 
+         * This method checks if the necessary file structure exists. If it does, nothing is done. 
+         * If the necessary file structure does not exist, the file structure is created to be interacted
+         * with by the rest of the application. 
          */
         private static void createFileStructure()
         {
@@ -66,8 +67,9 @@ namespace IT_Calculator
 
 
         /*
-         * The method will allow the user to select a file of their chosing
-         * and have it be stored as the active file in the application.
+         * The method will allow the user to select a file of their chosing to be read in and be
+         * operated on. 
+         * This method is only used for the "read" abilities of the application. 
          */
         public static void chooseFile()
         {
@@ -75,54 +77,103 @@ namespace IT_Calculator
         }
 
         /*
-         * This method write the operation compelted and results into a log file. 
+         * This method stores the operations compelted and results into temp linked list.
+         * This linked list is then read in later by a different method to write the lines for the text file(s).
+         * This is done to store all of the items into memory and once the session is exited or
+         * the user decides to start a new session, the application will then write all the 
+         * necessary components to the correct files. 
+         * 
+         * This method is specially for conversion operations. 
          */
-        public static void writeToLogFileConversion()
+        public static void storeForLogFileConversion(LinkedListStrings LL, string originalNum, string inputBase, string outputBase, string answer) 
         {
+            // build the string from the inputs
+            string writeString = $"conv {inputBase} to {outputBase} {originalNum} = {answer}";
 
+            // create a new nodeToAdd 
+            NodeString nodeToAdd = new NodeString(writeString);
+
+            // place the writeString into the passed linked list
+            LL.addNode(nodeToAdd);
         }
 
         /*
-         * This method writes the operation completed and results into the log file.
-         * This log file, for simplicity is created and stored in the documents folder. 
-         * This file is maintained on a session basis, if the session is ended, the file
-         * will not be cleared. If the sessions is created, the file will then be cleared.
-         * This is done to prevent clearing of data on the log file if the application crashes.
-         * Very useful resource for this method:
+         * This method stores the opeartions completed and results into a temp linked list. 
+         * This linked list is then read in later by a different method to write the lines to the text file(s).
+         * This is done to store all of the items into memory and once the session is exited or the user
+         * decides to start a new session, the application will then write all the necessary compnents to the correct files.
+         * Great rescourse used heavily:
          * https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/file-system/how-to-create-a-file-or-folder
-         * This method is only used for the writing of the log file.
-         *  
+         * 
+         * This method is specifically for binary arithmetic operations.
          */
-        public static void writeToLogFileArithmetic() //v num1, string num2, string operation
+        public static void storeForLogFileArithmetic(LinkedListStrings LL, int num1, string num2, string operation, string answer) 
         {
-            // temp values that will be used as parameters (needed for quick testing)
-            String num1 = "1010"; 
-            String num2 = "1011";
-            String operation = "Addition";
+            // build the string from the inputs
+            string writeString = $"arth {num1} {num2} {operation} = {answer}";
 
+            // create a new nodeToAdd 
+            NodeString nodeToAdd = new NodeString(writeString);
 
-            // specify the documnts folder as the top level folder and "IT-Calculator Logs" as the main file path
+            // place the writeString into the passed linked list
+            LL.addNode(nodeToAdd);
+        }
+
+        /*
+         * This method writes the information from both LinkedListString classes (arithmetic & conversion).
+         * 
+         */
+        public static void writeLogFilesToStorage(LinkedListStrings arithmeticLL, LinkedListStrings conversionLL)
+        {
+            // file path to the main logs directory
             string logFilePath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "IT-Calculator Logs");
-
-            // specify the "Session" sub folder in the "IT-Calculator Logs" file path 
+            // file path to the sessions directory
             string sessionsFilePath = System.IO.Path.Combine(logFilePath, "Sessions");
-
-            // specify "Previous Session Log.txt" file path
+            // file path to the previous sessions text file
             string prevSessionLog = System.IO.Path.Combine(logFilePath, "Previous Session Log.txt");
 
-            // call the creation method to ensure the file structure exists
+
+            // check that the file structure exists (call createFileStructrue)
             createFileStructure();
 
+            // open stream writer to write to the prevSession file
+            using (System.IO.StreamWriter prevSession = new System.IO.StreamWriter(prevSessionLog))
+            {
+                // write the date & time on the first line of the text file
+                prevSession.WriteLine(DateTime.Now.ToString("MM/dd/yyyy | HH:mm:ss"));
 
-           
-            // write the necessary information to the previous session log file
+                // arithmetic operations
+                if (arithmeticLL.length != 0)
+                {
+                    // write binary arithmetic on the first line
+                    prevSession.WriteLine("Bianry Arithmetic Operations");
+
+                    // loop to write each item in arithmeticLL
+                    for (int location = 0; location < arithmeticLL.length-1; location++)
+                    {
+                        prevSession.WriteLine(arithmeticLL.contentsAtIndex(location));
+                    }
+                }
+
+
+            }
+
+            // writing of any conversion operations
+            if (conversionLL.length != 0)
+            {
+                // lable first line with conversion
+
+            }
+            
+            
+            /*/ write the necessary information to the previous session log file
             using (System.IO.StreamWriter stream = new System.IO.StreamWriter(prevSessionLog))
             {
                 stream.Write("test");
             }
 
             // create a new file and write the same information to this sessions file
-
+            */
 
 
             /*
@@ -137,6 +188,7 @@ namespace IT_Calculator
                 // write the header for the file
                 outputFile.WriteLine(DateTime.Now.ToString("MM/dd/yyyy | HH:mm:ss"), "test");
             }*/
+
         }
     }
 }
